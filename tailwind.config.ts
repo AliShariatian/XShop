10dee120e830f1c5abeee50c9cb78bbe19698406
Ali Shariatian <shariatian92@gmail.com>
2024-03-01 02:28:23 +0330

    init: start project commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,16 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+   content: ["./src/pages/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}", "./src/app/**/*.{ts,tsx}"],
+   theme: {
+      extend: {
+         fontFamily: {
+            integralCF: ["var(--font-integralCF)"],
+            satoshi: ["var(--font-satoshi)"],
+         },
       },
-    },
-  },
-  plugins: [],
+   },
+   plugins: [],
 };
 export default config;
+
