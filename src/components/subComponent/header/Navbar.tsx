3cea9390920d0b6563ac34c4da49642ed50d01c0
Ali Shariatian <shariatian92@gmail.com>
2024-03-07 00:58:00 +0330

    💄 style: add sticky scroll to header

--- src/components/subComponent/header/Navbar.tsx
+++ src/components/subComponent/header/Navbar.tsx
@@ -17,67 +17,70 @@ const Navbar: FC = (): JSX.Element => {
    };
 
    return (
-      <header className="shadow-sm">
+      <>
          {/* TOP NAVBAR */}
          <TopNavbar onClick={closeTopNavbarHandler} close={closeTopNavbar} />
 
-         <Section parentClass="z-50 w-full flex h-20 items-center justify-between bg-light lg:h-24" sectionClass="flex items-center justify-between">
-            {/* Hamburger Menu Icon */}
-            <div className="flex gap-4">
-               <Image src={hamburgerMenuIcon} alt="open" width={20} height={20} className="mt-1 size-5 lg:hidden" />
+         {/* HEADER */}
+         <header className="sticky top-0 z-50 shadow-sm">
+            <Section parentClass="w-full flex h-20 items-center justify-between bg-light lg:h-24" sectionClass="flex items-center justify-between">
+               {/* Hamburger Menu Icon */}
+               <div className="flex gap-4">
+                  <Image src={hamburgerMenuIcon} alt="open" width={20} height={20} className="mt-1 size-5 lg:hidden" />
 
-               {/* LOGO */}
-               <div className="-mt-2">
-                  <h1 className="font-integralCF text-3xl uppercase">xshop</h1>
+                  {/* LOGO */}
+                  <div className="-mt-2">
+                     <h1 className="font-integralCF text-3xl uppercase">xshop</h1>
+                  </div>
                </div>
-            </div>
 
-            {/* NAVBAR ITEMS */}
-            <nav className="hidden lg:block">
-               <ul className="flex gap-5">
-                  <li className="flex items-center gap-2">
-                     <Link href="/">Shop</Link>
-                     <Image src={downArrowIcon} alt="open" width={10} height={10} className="size-[10px]" />
-                  </li>
-                  <li>
-                     <Link href="/">On Sale</Link>
-                  </li>
-                  <li>
-                     <Link href="/">New Arrivals</Link>
-                  </li>
-                  <li>
-                     <Link href="/">Brands</Link>
-                  </li>
-               </ul>
-            </nav>
+               {/* NAVBAR ITEMS */}
+               <nav className="hidden lg:block">
+                  <ul className="flex gap-5">
+                     <li className="flex items-center gap-2">
+                        <Link href="/">Shop</Link>
+                        <Image src={downArrowIcon} alt="open" width={10} height={10} className="size-[10px]" />
+                     </li>
+                     <li>
+                        <Link href="/">On Sale</Link>
+                     </li>
+                     <li>
+                        <Link href="/">New Arrivals</Link>
+                     </li>
+                     <li>
+                        <Link href="/">Brands</Link>
+                     </li>
+                  </ul>
+               </nav>
 
-            {/* SEARCH BAR */}
-            <div className="hidden w-2/5 rounded-full bg-grey-100 p-3 lg:flex">
-               <Image onClick={() => {}} src={searchIcon} alt="search" width={20} height={20} className="ml-2 size-5 cursor-pointer opacity-40" />
-               <input
-                  type="search"
-                  value={searchInput}
-                  onChange={(ev: React.ChangeEvent<HTMLInputElement>) => setSearchInput(ev.target.value)}
-                  className="ml-4 w-full bg-transparent pr-2 outline-none"
-                  maxLength={40}
-                  placeholder="Search for products..."
-               />
-            </div>
+               {/* SEARCH BAR */}
+               <div className="hidden w-2/5 rounded-full bg-grey-100 p-3 lg:flex">
+                  <Image onClick={() => {}} src={searchIcon} alt="search" width={20} height={20} className="ml-2 size-5 cursor-pointer opacity-40" />
+                  <input
+                     type="search"
+                     value={searchInput}
+                     onChange={(ev: React.ChangeEvent<HTMLInputElement>) => setSearchInput(ev.target.value)}
+                     className="ml-4 w-full bg-transparent pr-2 outline-none"
+                     maxLength={40}
+                     placeholder="Search for products..."
+                  />
+               </div>
 
-            {/* RIGHT ICONS */}
-            <div className="flex gap-3">
-               <Link href="/" className="lg:hidden">
-                  <Image onClick={() => {}} src={searchIcon} alt="search" width={20} height={20} />
-               </Link>
-               <Link href="/">
-                  <Image src={basketIcon} alt="basket" width={20} height={20} className="size-5" />
-               </Link>
-               <Link href="/">
-                  <Image src={accountIcon} alt="account" width={20} height={20} className="size-5" />
-               </Link>
-            </div>
-         </Section>
-      </header>
+               {/* RIGHT ICONS */}
+               <div className="flex gap-3">
+                  <Link href="/" className="lg:hidden">
+                     <Image onClick={() => {}} src={searchIcon} alt="search" width={20} height={20} />
+                  </Link>
+                  <Link href="/">
+                     <Image src={basketIcon} alt="basket" width={20} height={20} className="size-5" />
+                  </Link>
+                  <Link href="/">
+                     <Image src={accountIcon} alt="account" width={20} height={20} className="size-5" />
+                  </Link>
+               </div>
+            </Section>
+         </header>
+      </>
    );
 };
 
