cea4543e0be3a2c442ad8059a63c7f8b051cc85b
Ali Shariatian <shariatian92@gmail.com>
2024-03-11 21:48:50 +0330

    🐛 fix: add breakpoint to slider

--- src/components/homePage/Vitrine.tsx
+++ src/components/homePage/Vitrine.tsx
@@ -47,44 +47,47 @@ const Vitrine: FC<PropsType> = ({ title, sortBy, order, buttonHref }): JSX.Eleme
    return (
       <Section parentClassName="py-[5.62rem]" sectionClassName="flex flex-col items-center gap-16">
          <h2 className="font-integralCF text-5xl">{title}</h2>
-         <Swiper
-            draggable
-            breakpoints={{
-               0: {
-                  slidesPerView: 1,
-                  spaceBetween: -15,
-               },
-               500: {
-                  slidesPerView: 2,
-                  spaceBetween: -15,
-               },
 
-               1280: {
-                  slidesPerView: 3,
-                  spaceBetween: -115,
-               },
-               1600: {
-                  slidesPerView: 4,
-                  spaceBetween: 0,
-               },
-            }}
-            className="!grid grid-cols-4"
-         >
-            {products.length
-               ? products.map((item: ProductsType) => (
-                    <SwiperSlide key={item.id}>
-                       <ProductCard {...item} />
-                    </SwiperSlide>
-                 ))
-               : //  Skeleton loading
-                 Array(PRODUCT_SHOW_COUNT)
-                    .fill(0)
-                    .map((_, index) => (
-                       <SwiperSlide key={index}>
-                          <ProductCardSkeleton />
-                       </SwiperSlide>
-                    ))}
-         </Swiper>
+         {products.length ? (
+            <Swiper
+               draggable
+               breakpoints={{
+                  0: {
+                     slidesPerView: 1,
+                     spaceBetween: -15,
+                  },
+                  500: {
+                     slidesPerView: 2,
+                     spaceBetween: -15,
+                  },
+
+                  1280: {
+                     slidesPerView: 3,
+                     spaceBetween: -115,
+                  },
+                  1600: {
+                     slidesPerView: 4,
+                     spaceBetween: 0,
+                  },
+               }}
+               className="!grid grid-cols-4"
+            >
+               {products.map((item: ProductsType) => (
+                  <SwiperSlide key={item.id}>
+                     <ProductCard {...item} />
+                  </SwiperSlide>
+               ))}
+            </Swiper>
+         ) : (
+            //  Skeleton loading
+            <div className="flex gap-4 max-md:[&>*:nth-child(1)]:hidden max-lg:[&>*:nth-child(2)]:hidden max-xl:[&>*:nth-child(3)]:hidden">
+               {Array(PRODUCT_SHOW_COUNT)
+                  .fill(0)
+                  .map((_, index) => (
+                     <ProductCardSkeleton key={index} />
+                  ))}
+            </div>
+         )}
 
          <Link href={buttonHref}>
             <Button bgColor="white" borderColor="border-grey-100" py="py-3">
