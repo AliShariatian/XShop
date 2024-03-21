d5209b8931a5e76ce4829446a7367bad91c3921d
Ali Shariatian <shariatian92@gmail.com>
2024-03-22 02:46:34 +0330

    add product filter start config

--- src/app/(pages)/products/Filters.tsx
+++ src/app/(pages)/products/Filters.tsx
@@ -1,7 +1,19 @@
 import { FC } from "react";
+import { filter } from "@/public/img";
+import Image from "next/image";
 
 const Filters: FC = (): JSX.Element => {
-   return <div className="h-full w-1/4 bg-red-400 max-xl:hidden">filter</div>;
+   return (
+      <div className="h-full w-1/4 rounded-20 border p-5 max-xl:hidden">
+         {/* Title */}
+         <div className="flex items-center justify-between">
+            <h4 className="text-xl font-bold">Filters</h4>
+            <Image src={filter} alt="filter" width={20} height={20} className="size-5" />
+         </div>
+
+         <hr className="my-5" />
+      </div>
+   );
 };
 
 export default Filters;
