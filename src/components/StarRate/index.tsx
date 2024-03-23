0d6acd348b693ffcd3be76bdf898049aee189afe
Ali Shariatian <shariatian92@gmail.com>
2024-03-23 22:54:58 +0330

    💄 style: change stare rating number config

--- src/components/StarRate/index.tsx
+++ src/components/StarRate/index.tsx
@@ -2,33 +2,44 @@ import { FC } from "react";
 import { StarRatePropsType } from "./type";
 import cn from "@/utils/cn";
 import Image from "next/image";
-import { starFullRate, starEmptyRate } from "@/public/img";
+import { starFullRate, starEmptyRate, starHalfRate } from "@/public/img";
+
+const ALL_STAR_COUNT = 5;
+
+const starImage = (index: number, type: string): JSX.Element => {
+   return <Image key={index} src={type} width={50} height={50} alt="star" className="size-5" />;
+};
 
 const StarRate: FC<StarRatePropsType> = ({ rate, showEmptyStar = true, showRateNumber = true, className }): JSX.Element => {
+   // Star filling config
+   const Star = Array.from({ length: ALL_STAR_COUNT }, (_, index) => {
+      const number = index + 0.5;
+
+      return (
+         <span key={index}>
+            {rate >= index + 1
+               ? starImage(index, starFullRate)
+               : rate >= number
+                 ? starImage(index, starHalfRate)
+                 : showEmptyStar && starImage(index, starEmptyRate)}
+         </span>
+      );
+   });
+
+   // Rate number rounding config
+   const convertToNearestHalf = (rate: number) => {
+      return Math.floor(rate * 2) / 2;
+   };
+
    return (
       <div className="flex gap-3">
-         <div className={cn("flex gap-[2px]", className)}>
-            {/* Fill star */}
-            {Array(Math.ceil(rate))
-               .fill(0)
-               .map((_, index) => (
-                  <Image key={index} src={starFullRate} width={50} height={50} alt="star" className="size-5" />
-               ))}
-
-            {/* Empty star */}
-            {showEmptyStar &&
-               Array(5 - Math.ceil(rate))
-                  .fill(0)
-                  .map((_, index) => (
-                     <Image key={index} src={starEmptyRate} width={50} height={50} alt="star" className="size-5" />
-                  ))}
-         </div>
+         <div className={cn("flex gap-[2px]", className)}>{Star}</div>
 
          {/* Rate number */}
          {showRateNumber && (
             <div>
-               <span>{Math.ceil(rate)}/</span>
-               <span className="text-gray-400">5</span>
+               <span>{convertToNearestHalf(rate)}</span>
+               <span className="text-gray-400">/{ALL_STAR_COUNT}</span>
             </div>
          )}
       </div>
