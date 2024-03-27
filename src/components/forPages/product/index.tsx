464e69065fb000878e1143a521ee72689b168ae7
Ali Shariatian <shariatian92@gmail.com>
2024-03-28 00:01:01 +0330

    🐛 fix: add redirect to 404 page when product url is invalid

--- src/components/forPages/product/index.tsx
+++ src/components/forPages/product/index.tsx
@@ -3,15 +3,19 @@
 import { FC } from "react";
 import { usePathname } from "next/navigation";
 import { toast } from "react-toastify";
+import { notFound } from "next/navigation";
 import GetSingleProducts from "@/services/reactQuery/singleProducts";
 import { Breadcrumb, Section, ProductDetail, ProductImageGallery, ProductPageSkeleton } from "@/components";
 
 const SingleProductComponents: FC = (): JSX.Element => {
    const currentProduct = usePathname().split("/").at(-1) as string;
-
    const { data, isLoading, isError, error } = GetSingleProducts(currentProduct);
-
    const product = data?.[0]!;
+
+   // If url invalid, redirect to 404 page
+   if (data?.length === 0) {
+      return notFound();
+   }
 
    // Show toast message when error to fetch data from server
    isError && toast.error(error.message);
