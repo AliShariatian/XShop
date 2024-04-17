d9432af9a16b4dd184e73c2af310316645e5fe0b
Ali Shariatian <shariatian92@gmail.com>
2024-04-17 20:35:26 +0330

    change api base url

--- src/services/axios/configs/baseURL.ts
+++ src/services/axios/configs/baseURL.ts
@@ -1,3 +1,4 @@
-const baseURL: string = "http://localhost:8080/" as const;
+// const baseURL: string = "http://localhost:8080/" as const;
+const baseURL: string = "https://api.xshop.shariatian.ir/" as const;
 
 export default baseURL;
