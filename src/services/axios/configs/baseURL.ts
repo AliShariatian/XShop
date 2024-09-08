e0c680076a16a339c3bacc69f64ce5c158c83dff
Ali Shariatian <shariatian92@gmail.com>
2024-09-08 20:53:50 +0330

    change api baseURL

--- src/services/axios/configs/baseURL.ts
+++ src/services/axios/configs/baseURL.ts
@@ -1,4 +1,4 @@
 const baseURL: string =
-   process.env.NODE_ENV === "development" ? ("http://localhost:8080/" as const) : ("https://api.xshop.shariatian.ir/" as const);
+   process.env.NODE_ENV === "development" ? ("http://localhost:8080/" as const) : ("https://api-xshop.liara.run/" as const);
 
 export default baseURL;
